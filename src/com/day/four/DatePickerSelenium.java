89ed0097ebd1de57e5be38be21393b2a52db8361
Ravi Kant Pal <35272306+ravikantpal1408@users.noreply.github.com>
2022-02-26 18:51:57 +0530

    beginning of WebElement Part 4

---
+++ src/com/day/four/DatePickerSelenium.java
@@ -0,0 +1,56 @@
+package com.day.four;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Calendar;
+
+public class DatePickerSelenium {
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\SeleniumDriver\\chromedriver.exe");
+        driver = new ChromeDriver();                // chrome driver
+        driver.get("https://goo.gl/RVdKM9");        // get url
+        driver.manage().window().maximize();        // maximise window
+
+        String year = "2019";
+        String month = "August";
+        String date = "14";
+
+        driver.findElement(By.xpath("this some ex-path to datepicker")).click();            // this will open and focus on the datepicker
+
+        selectCurrentDate();
+        selectPastDate(date, month);
+        selectFutureDate(date, month);
+    }
+
+    static void selectCurrentDate() {
+        Calendar calendar = Calendar.getInstance();                            // default JAVA calendar instance
+        int integerDate = calendar.get(Calendar.DATE);                         // return current date in integer format
+        String date = String.valueOf(integerDate);                             // get date in string format using integer value
+        driver.findElement(By.linkText(date));                                 // now select the date in datepicker
+
+    }
+
+    static void selectPastDate(String d, String m) {
+
+    }
+
+    static void selectFutureDate(String d, String m) {
+        Calendar calendar = Calendar.getInstance();
+        int currentMonth = calendar.get(Calendar.MONTH);                        // return current date in integer format
+
+        for (int i = currentMonth; i >= 1; i++) {
+            driver.findElement(By.xpath("date picker carrot icon")).click();            // next arrow
+            String month = driver.findElement(By.xpath("this is text")).getText();      //
+            if(month.equals(m)) {
+                driver.findElement(By.linkText(d)).click();
+                break;
+            }
+
+        }
+    }
+}
---
+++ src/com/day/three/HandleWindows.java
@@ -0,0 +1,33 @@
+package com.day.three;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class HandleWindows {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\Tools\\SeleniumDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://demo.automationtesting.in/Windows.html");
+        driver.manage().window().maximize();
+
+        //String handleVal = driver.getWindowHandle();
+        //System.out.println(handleVal);
+
+        driver.findElement(By.xpath("//*[@id=\"Tabbed\"]/a/button")).click();
+        Set<String> handleValues = driver.getWindowHandles();
+
+        for(var items : handleValues) {
+            //System.out.println(items);
+            String title = driver.switchTo().window(items).getTitle();
+            System.out.println(title);
+            if(title.equals("Selenium")) {
+                driver.close();
+            }
+        }
+
+    }
+}
